aaeca0f84091b8d17146f426f1cf28d24e143b29
LathaRaoKR <latharaosalanke1717@gmail.com>
2025-05-11 12:16:54 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,28 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import connectDB from './config/mongodb.js';
-import connectCloudinary from './config/cloudinary.js';
-import userRouter from './routes/userRoute.js';
-import productRouter from './routes/productRoute.js';
-import cartRouter from './routes/cartRoute.js';
-import orderRouter from './routes/orderRoute.js';
+import express from "express";
+import cors from "cors";
+import serverless from "serverless-http";
+import "dotenv/config";
+import connectDB from "../config/mongodb.js";
+import connectCloudinary from "../config/cloudinary.js";
+import userRouter from "../routes/userRoute.js";
+import productRouter from "../routes/productRoute.js";
+import cartRouter from "../routes/cartRoute.js";
+import orderRouter from "../routes/orderRoute.js";
 
-
-//App Config
-const app=express();
-const port = process.env.PORT || 4000;
+const app = express();
 connectDB();
 connectCloudinary();
-//middlewares
-app.use(express.json());
-app.use(cors());
 
-//api end 'points
-app.use('/api/user',userRouter)
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
 
-app.get('/',(req,res)=>{
-    res.send("API Working")
-
-})
-
-
-
-
-app.listen(port,()=>console.log('Server started on port:'+port))
+export default serverless(app);
